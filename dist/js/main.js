1409c5526d95fac3bc376a9b0772cdbe502bf2d9
Augustus Makori <makoriaugust.am@gmail.com>
2020-06-08 04:45:22 +0300

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 
-const navItems = document.querySelectorAll("nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Show Initial state of Menu
 let showMenu = false;
